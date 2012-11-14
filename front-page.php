c0c81fefc4fcf50d5f2171792e4337c7ee07c00a
Terry Sutton <saltcod@gmail.com>
2012-11-13 20:47:51 -0330

    Add triangular logo

--- front-page.php
+++ front-page.php
@@ -12,12 +12,15 @@
  */
 
 get_header(); ?>
-
+<div class="secondary">sam</div>
 		<div id="primary" class="content-area">
 			<div id="content" class="site-content" role="main">
-adf
-			
-					
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content .site-content -->
 		</div><!-- #primary .content-area -->
